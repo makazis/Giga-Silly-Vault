68a40d37175bc967815ccec68e4d6beca7b30dd3
makazis <labdiennabagi@gmail.com>
2026-03-10 23:53:09 +0200

    vault backup: 2026-03-10 23:53:09

---
+++ Compiler.py
@@ -0,0 +1,12 @@
+import os
+total=""
+path1="Wendigoon D&D/Theory D&D"
+path2="Wendigoon D&D/HFY Power Armor"
+for root,dirs,files in os.walk(path2):
+    for file in files:
+        if file.endswith(".md"):
+            path=os.path.join(root,file)
+            with open(path,"r") as fs:
+                total+=f"\nFile: {path}\n\n{fs.read()}\n"
+with open("total.txt","w") as f:
+    f.write(total)
